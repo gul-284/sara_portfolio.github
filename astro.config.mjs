1906efd5c819c722481a1d68066218fff05af1a1
gul-284 <sg400317@gmail.com>
2026-04-30 02:51:06 +0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
-// @ts-check
-import { defineConfig } from "astro/config";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-  },
-});
+  // Replace 'gul-284' with your GitHub username
+  site: 'https://gul-284.github.io', 
+  // Replace 'sara_portfolio.github' with your exact repo name
+  base: '/sara_portfolio.github',
+})
